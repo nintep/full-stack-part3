45522c26daac35e8c781ccd06731feaf9d7dfa66
Nina Tepponen <nina.tepponen@aalto.fi>
2022-02-08 17:35:17 +0200

    Add frontend

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # full-stack-part3
+
+Linkki sovellukseen:
+https://secret-woodland-01722.herokuapp.com/api/persons
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(express.static('build'))
 
 //app.use(morgan('tiny'))
 
